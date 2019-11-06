f14165f02687541845d61ff039fdeea4fce476c0
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-11-06 10:53:35 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Select DOM Items
+const headerBtn = document.querySelector('.header-btn')
+const menu = document.querySelector('.menu')
+const menuNav = document.querySelector('.menu-nav')
+const menuBranding = document.querySelector('.menu-branding')
+const navItems = document.querySelectorAll('.nav-item')
+
+//Set Initial State of Menu
+let showMenu = false;
+
+headerBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        headerBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //Set Menu State
+        showMenu = true;
+    } else {
+        headerBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //Set Menu State
+        showMenu = false;
+    }
+}
